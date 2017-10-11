7bc89c6f974567983a080d039036f677011279e6
ManyiLi12345 <manyi.li.12345@gmail.com>
2017-10-11 10:22:28 -0700

    js

--- js/custom.js
+++ js/custom.js
@@ -140,18 +140,6 @@ function initMap() {
             ]
         }
     ];
-    map = new google.maps.Map(document.getElementById('map'), {
-       center: {lat: 23.822724694190565, lng: 88.7800669670105},
-        // center: myCenter, 23.830262191202287 88.76633405685425
-        zoom: 11,
-        maptype: 'roadmap',
-        styles: mapStyle
-    });
-    google.maps.event.addListener(map,'click',function(e) {
-    console.log(e.latLng.lat(),e.latLng.lng())
-                     document.getElementById('latlongclicked').value = e.latLng.lat()
-                     document.getElementById('lotlongclicked').value =  e.latLng.lng()
-                 });
 }
 
 function showPage(bt, pagename) {
